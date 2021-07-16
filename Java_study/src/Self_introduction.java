569b08d296ec2e9bd46dc881029fb66de5140ee8
watersidewood <jslim8120@naver.com>
2021-07-16 21:37:16 +0900

    self_introduction

---
+++ Java_study/src/Self_introduction.java
@@ -0,0 +1,20 @@
+public class Self_introduction {
+    public static void main(String[] args) {
+        String name;
+        int year, month, day;
+        boolean married;
+
+        // 값 대입
+        name = "코드잇";
+        year = 2015;
+        month = 7;
+        day = 20;
+        married = false;
+
+        // 자기 소개
+        System.out.println("이름: " + name);
+        System.out.println("생일: " + year + "년 " + month + "월 " + day + "일");
+        System.out.println("결혼: " + married);
+
+    }
+}
