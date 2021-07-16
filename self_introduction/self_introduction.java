28e585cd7ce7f0cb9e39ab29c216f1dbee616da1
watersidewood <87155909+watersidewood@users.noreply.github.com>
2021-07-16 22:05:49 +0900

    Add files via upload

---
+++ self_introduction/self_introduction.java
@@ -0,0 +1,22 @@
+package self_introduction;
+
+public class self_introduction {
+    public static void main(String[] args) {
+        String name;
+        int year, month, day;
+        boolean married;
+
+        // 값 대입
+        name = "코드잇";
+        year = 2015;
+        month = 7;
+        day = 20;
+        married = false;
+
+        // 자기 소개
+        System.out.println("이름: " + name);
+        System.out.println("생일: " + year + "년 " + month + "월 " + day + "일");
+        System.out.println("결혼: " + married);
+
+    }
+}
